5e574485114d6b55bce75c5b3ad5369dab5739ee
Thomas Weld <thomas.weld@gmail.com>
2017-12-17 11:57:17 -0500

    redux dev tools working

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(timeSlots);
+const store = createStore(
+  timeSlots,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
